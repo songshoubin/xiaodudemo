7f3c19e41acc48e4db8b52537b1fd83500a4e619
songshoubin <38982928+songshoubin@users.noreply.github.com>
2019-07-24 15:55:05 +0800

    调优

--- api/src/main/java/com/xiaodu/ApplicationRun.java
+++ api/src/main/java/com/xiaodu/ApplicationRun.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.annotation.Resource;
 
 /**
- * CPS版权保护系统启动类
+ * 系统启动类
  *
  * @author songshoubin
  * @date 2019-5-15
@@ -19,22 +19,12 @@ import javax.annotation.Resource;
 @EnableTransactionManagement
 public class ApplicationRun implements CommandLineRunner {
 
-
-    @Resource
-    private TestUserMapper testUserMapper;
-
-    @Resource
-    private ElaJobMapper elaJobMapper;
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRun.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-//        elasticJobService.scanAddJob();
-//        taskJobMapper.selectByPrimaryKey(1L);
-        System.out.println(elaJobMapper.selectByPrimaryKey(1L).toString());
-        System.out.println(testUserMapper.selectByPrimaryKey(1).toString());
+
     }
 }
